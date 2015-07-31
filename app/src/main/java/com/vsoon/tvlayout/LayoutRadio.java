2a8bb6644b5070f32da0215abf4e3c96f7f4d1d4
liangjunfeng <liangjunfeng@ipmacro.com>
2015-07-31 13:43:52 +0800

    统一设置分辨率的方法名

--- app/src/main/java/com/vsoon/tvlayout/LayoutRadio.java
+++ app/src/main/java/com/vsoon/tvlayout/LayoutRadio.java
@@ -22,33 +22,38 @@ public class LayoutRadio {
     public static float RADIO_HEIGHT = 1.0f;
     public static float RADIO_AVERAGE = 1.0f;
 
+    /**
+     * 初始化比率，标准宽和高采用的是 1280 * 720
+     * @param activity
+     */
     public static void initRadio(Activity activity) {
-        Display display = activity.getWindowManager().getDefaultDisplay();
-        Point point = new Point();
-        display.getRealSize(point);
-        REAL_WIDTH = point.x;
-        REAL_HEIGHT = point.y;
-        RADIO_WIDTH = REAL_WIDTH / (float) STANDARD_WIDTH;
-        RADIO_HEIGHT = REAL_HEIGHT / (float) STANDARD_HEIGHT;
-        RADIO_AVERAGE = (RADIO_WIDTH + RADIO_HEIGHT) / 2;
-        Log.e(TAG, "REAL_WIDTH = " + REAL_WIDTH
-                + ", REAL_HEIGHT = " + REAL_HEIGHT
-                + ", RADIO_WIDTH = " + RADIO_WIDTH
-                + ", RADIO_HEIGHT = " + RADIO_HEIGHT
-                + ", RADIO_AVERAGE = " + RADIO_AVERAGE);
+        initRadio(activity, STANDARD_WIDTH, STANDARD_HEIGHT);
     }
 
     /**
-     * 设置标准，如果不设置的话，默认的标准宽为 1280，默认的标准高为 720
+     * 初始化比率，根据传入的值来设置标准的宽和高，传入的宽和高
+     * 必须大于 0，否则会抛出 IllegalArgumentException 异常
      * @param activity
      * @param standardWidth
      * @param standardHeight
      */
-    public static void initStandard(Activity activity, int standardWidth, int standardHeight) {
+    public static void initRadio(Activity activity, int standardWidth, int standardHeight) {
         if (standardWidth > 0 && standardHeight > 0) {
             STANDARD_WIDTH = standardWidth;
             STANDARD_HEIGHT = standardHeight;
-            initRadio(activity);
+            Display display = activity.getWindowManager().getDefaultDisplay();
+            Point point = new Point();
+            display.getRealSize(point);
+            REAL_WIDTH = point.x;
+            REAL_HEIGHT = point.y;
+            RADIO_WIDTH = REAL_WIDTH / (float) STANDARD_WIDTH;
+            RADIO_HEIGHT = REAL_HEIGHT / (float) STANDARD_HEIGHT;
+            RADIO_AVERAGE = (RADIO_WIDTH + RADIO_HEIGHT) / 2;
+            Log.e(TAG, "REAL_WIDTH = " + REAL_WIDTH
+                    + ", REAL_HEIGHT = " + REAL_HEIGHT
+                    + ", RADIO_WIDTH = " + RADIO_WIDTH
+                    + ", RADIO_HEIGHT = " + RADIO_HEIGHT
+                    + ", RADIO_AVERAGE = " + RADIO_AVERAGE);
         } else {
             throw new IllegalArgumentException("Both standard width and height should be positive.");
         }
